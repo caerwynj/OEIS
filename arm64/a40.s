4e683bf301b2fa1fe24bbd4d47e95f7126cd950c
caerwyn <caerwynj@gmail.com>
2023-06-11 11:14:37 -0400

    a40

--- arm64/a40.s
+++ arm64/a40.s
@@ -2,15 +2,11 @@
 .global genseq
 genseq:
 	mov x3, xzr
-	mov	x4, #1
-	mov	x5, #1
+	mov	x4, #2
+	mov	x5, #3
 loop:
 	str	x4, [x0, x3, lsl #3]
 	add	x3, x3,#1
-	adds	x6, x4, x5
-	bvs	end
-	mov	x4, x5
-	mov	x5, x6
 	cmp	x3, x1
 	bne	loop
 end:
